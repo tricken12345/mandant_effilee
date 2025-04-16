9efcf52375bc7f8fd9368f151f7973277fdb5b85
Tobias Ricken <tobias.ricken@iqdigital.de>
2025-04-16 11:34:07 +0200

    feat: Initial commit

---
+++ gdpr_cnfg/script_cnfg/prebid-placementIds-effilee.js
@@ -0,0 +1,126 @@
+function getPlacementIdYieldlab(placement) {
+    let si = 0;
+    switch (placement) {
+      case 'iqdeffilee-outstream':
+        si = 16489873;
+        break;
+      case 'iqdeffilee-iqadtile1':
+        si = 16489850;
+        break;
+      case 'iqdeffilee-iqadtile21':
+        si = 16489856;
+        break;
+      case 'iqdeffilee-iqadtile20':
+        si = 16489855;
+        break;
+      case 'iqdeffilee-sitebarr':
+        si = 16489871;
+        break;
+      case 'iqdeffilee-sitebarl':
+        si = 16489872;
+        break;
+      case 'iqdeffilee-iqadtile4':
+        si = 16489857;
+        break;
+      case 'iqdeffilee-iqadtile41':
+        si = 16489858;
+        break;
+      case 'iqdeffilee-iqadtile42':
+        si = 16489859;
+        break;
+      case 'iqdeffilee-iqadtile43':
+        si = 16489860;
+        break;
+      case 'iqdeffilee-iqadtile5':
+        si = 16489861;
+        break;
+      case 'iqdeffilee-iqadtile51':
+        si = 16489862;
+        break;
+      case 'iqdeffilee-iqadtile52':
+        si =16489863;
+        break;
+      case 'iqdeffilee-iqadtile53':
+        si = 16489864;
+        break;    
+      case 'iqdeffilee-iqadtile8':
+        si = 16489865;
+        break;    
+      case 'iqdeffilee-iqadtile11':
+        si = 16489851;
+        break;
+      case 'iqdeffilee-iqadtile12':
+        si = 16489852;
+        break;
+      case 'iqdeffilee-iqadtile13':
+        si = 16489853;
+        break;
+      case 'iqdeffilee-iqadtile14':
+        si = 16489854;
+        break;
+      case 'iqdeffilee-iqadtile99':
+        si = 16489866;
+        break;
+      case 'iqdeffilee-iqadtile991':
+        si = 16489867;
+        break;
+      case 'iqdeffilee-iqadtile992':
+        si = 16489868;
+        break;
+      case 'iqdeffilee-iqadtile993':
+        si = 16489869;
+        break;
+      case 'iqdeffilee-iqadtile16':
+        si = 16489870;
+        break;
+      case 'iqdeffilee_mob-outstream':
+        si = 16489889;
+        break;    
+      case 'iqdeffilee_mob-iqadtile1':
+        si = 16489874;
+        break;
+      case 'iqdeffilee_mob-iqadtile3':
+        si = 16489875;
+        break;
+      case 'iqdeffilee_mob-iqadtile4':
+        si = 16489876;
+        break;
+      case 'iqdeffilee_mob-iqadtile41':
+        si = 16489877;
+        break;
+      case 'iqdeffilee_mob-iqadtile42':
+        si = 16489878;
+        break;
+      case 'iqdeffilee_mob-iqadtile43':
+        si = 16489879;
+        break;
+      case 'iqdeffilee_mob-iqadtile5':
+        si = 16489880;
+        break;    
+      case 'iqdeffilee_mob-iqadtile51':
+        si = 16489881;
+        break;
+      case 'iqdeffilee_mob-iqadtile52':
+        si = 16489882;
+        break;
+      case 'iqdeffilee_mob-iqadtile53':
+        si = 16489883;
+        break;    
+      case 'iqdeffilee_mob-iqadtile8':
+        si = 16489884;
+        break;    
+      case 'iqdeffilee_mob-iqadtile99':
+        si = 16489885;
+        break;
+      case 'iqdeffilee_mob-iqadtile991':
+        si = 16489886;
+        break;
+      case 'iqdeffilee_mob-iqadtile992':
+        si = 16489887;
+        break;
+      case 'iqdeffilee_mob-iqadtile993':
+        si = 16489888;
+        break;
+    }
+    return si;
+  }
