e8c542c56943ab279ced9842bdfd91a7a13f2f4f
Tobias Ricken <tobias.ricken@iqdigital.de>
2025-04-17 11:31:11 +0200

    feat: Iniital commit

---
+++ gdpr_cnfg/script_cnfg/containerModell_effilee_digital.js
@@ -0,0 +1,149 @@
+let _iqadtypeMob = AdController.getConfig().resolve('').getVar('iqadtype') == 'mew' ? true : false;
+
+window.IQD_varPack = window.IQD_varPack || {};
+window.IQD_varPack.containerModell = {
+    adContainerAlreadyInserted: false,
+    adTileMatchingList: {
+
+        default: {
+            pos_header: {
+                desktop: ["iqadtileOOP", "iqadtile111"],
+                mobile: ["iqadtileOOP"],
+            },
+            pos_1: {
+                desktop: "topBanner",
+                mobile: "iqadtile1",
+            },
+            pos_2: {
+                desktop: "iqadtile4",
+                mobile: "iqadtile3",
+            },
+            pos_3: {
+                desktop: "iqadtile5",
+                mobile: "iqadtile4",
+            },
+            pos_4: {
+                desktop: "iqadtile41",
+                mobile: "iqadtile5",
+            },
+            pos_5: {
+                desktop: "iqadtile51",
+                mobile: "iqadtile41",
+            },
+            pos_6: {
+                desktop: "iqadtile42",
+                mobile: "iqadtile51",
+            },
+            pos_7: {
+                desktop: "iqadtile52",
+                mobile: "iqadtile42",
+            },
+            pos_8: {
+                desktop: "iqadtile43",
+                mobile: "iqadtile52",
+            },
+            pos_marketplace: {
+                desktop: "iqadtile15",
+                mobile: "iqadtile15",
+            },
+            pos_9: {
+                desktop: "iqadtile53",
+                mobile: "iqadtile43",
+            },
+            pos_footer: {
+                desktop: "iqadtile16",
+                mobile: "iqadtile8",
+            },
+        },
+        index: {
+            pos_2: {
+                desktop: "iqadtile4",
+                mobile: "iqadtile3",
+            },
+            pos_3: {
+                desktop: "iqadtile5",
+                mobile: "iqadtile4",
+            },
+            pos_4: {
+                desktop: "iqadtile41",
+                mobile: "iqadtile5",
+            },
+            pos_5: {
+                desktop: "iqadtile51",
+                mobile: "iqadtile41",
+            },
+        },
+        artikel: {
+            pos_2: {
+                desktop: "iqadtile8",
+                mobile: "iqadtile3",
+            },
+            pos_3: {
+                desktop: "iqadtile4",
+                mobile: "iqadtile4",
+            },
+            pos_4: {
+                desktop: "iqadtile99",
+                mobile: "iqadtile99",
+            },
+            pos_5: {
+                desktop: "iqadtile41",
+                mobile: "iqadtile5",
+            },
+            pos_6: {
+                desktop: "iqadtile991",
+                mobile: "iqadtile41",
+            },
+            pos_7: {
+                desktop: "iqadtile42",
+                mobile: "iqadtile991",
+            },
+            pos_8: {
+                desktop: "iqadtile992",
+                mobile: "iqadtile51",
+            },
+            pos_9: {
+                desktop: "iqadtile43",
+                mobile: "iqadtile42",
+            },
+            pos_10: {
+                desktop: "iqadtile993",
+                mobile: "iqadtile992",
+            },
+            pos_11: {
+                desktop: "iqadtile5",
+                mobile: "iqadtile52",
+            },
+            pos_12: {
+                desktop: "iqadtile51",
+                mobile: "iqadtile43",
+            },
+            pos_13: {
+                desktop: "iqadtile52",
+                mobile: "iqadtile993",
+            },
+            pos_14: {
+                desktop: "iqadtile53",
+                mobile: "iqadtile53",
+            },
+        },
+    },
+    iqdTopBannerList: ["iqadtile1", "iqadtile20", "iqadtile21"],
+    iqdTopBannerArray: [
+        "iqd_mainAd", [
+            "iqd_align_Ad", [
+                ["iqd_leftAd", [
+                    "iqadtile20"
+                ]],
+                ["iqd_topAd", [
+                    "iqadtile1"
+                ]],
+                ["iqd_rightAd", [
+                    "iqadtile21"
+                ]],
+
+            ]
+        ]
+    ]
+
+};
